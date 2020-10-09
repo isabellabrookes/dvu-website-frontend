b5d7c2a2d0d22054ad36e9d5e98950317e2c5df6
Isabella Brookes <hello@isabellabrookes.com>
2020-10-09 22:42:28 +0200

    :bug: #1 Fixed Menu sliding animation

--- src/Components/Global/Navigation.jsx
+++ src/Components/Global/Navigation.jsx
@@ -4,47 +4,61 @@ import { Link } from "react-router-dom";
 import style from "./Navigation.module.css";
 import RedButton from "./RedButton";
 
+function DisplayMenu(props) {
+  console.log("animatedMenu", props.animateMenu);
+  const overlayAnimation = props.animateMenu && style.showTransparentOverlay;
+  const menuAnimation =
+    typeof props.animateMenu === "undefined" ||
+    (props.animateMenu ? style.slideIn : style.slideOut);
+
+  return (
+    <>
+      <div
+        onClick={props.toggleMenu}
+        className={`${style.transparentOverlay} ${overlayAnimation}`}
+      ></div>
+      <div id="puppies" className={`${style.menu} ${menuAnimation}`}>
+        <Link className={style.menuItem} to="/">
+          HOME
+        </Link>
+        <Link className={style.menuItem} to="/aboutus">
+          ABOUT US
+        </Link>
+        <Link className={style.menuItem} to="/events">
+          EVENTS
+        </Link>
+        <Link className={style.menuItem} to="/sponsors">
+          SPONSORS
+        </Link>
+        <Link className={style.menuItem} to="/media">
+          MEDIA
+        </Link>
+        <Link className={style.menuItem} to="/leaderboard">
+          LEADERBOARD
+        </Link>
+        <Link to="/login">
+          <RedButton
+            text={props.isLoggedIn ? "Account" : "Log in"}
+            iconClassName="far fa-user"
+          ></RedButton>
+        </Link>
+      </div>
+    </>
+  );
+}
+
 function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [animateMenu, setAnimateMenu] = useState();
 
-  function DisplayMenu(props) {
-    const animation = props.isOpen ? style.slideInRight : style.slideOutRight;
-    const overlay = props.isOpen
-      ? style.showTransparentOverlay
-      : style.hideTransparentOverlay;
-
-    return (
-      <>
-        <div className={style.menu + " " + animation}>
-          <Link className={style.menuItem} to="/">
-            HOME
-          </Link>
-          <Link className={style.menuItem} to="/aboutus">
-            ABOUT US
-          </Link>
-          <Link className={style.menuItem} to="/events">
-            EVENTS
-          </Link>
-          <Link className={style.menuItem} to="/sponsors">
-            SPONSORS
-          </Link>
-          <Link className={style.menuItem} to="/media">
-            MEDIA
-          </Link>
-          <Link className={style.menuItem} to="/leaderboard">
-            LEADERBOARD
-          </Link>
-          <Link to="/login">
-            <RedButton
-              text={isLoggedIn ? "Account" : "Log in"}
-              iconClassName="far fa-user"
-            ></RedButton>
-          </Link>
-        </div>
-        <div onClick={() => setIsMenuOpen(false)} className={overlay}></div>
-      </>
-    );
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+    if (!isMenuOpen) {
+      setAnimateMenu(true);
+    } else {
+      setAnimateMenu(false);
+    }
   }
 
   return (
@@ -53,18 +67,23 @@ function Navigation() {
         <img
           className={style.dvuLogo}
           src="./assets/images/logo/dvu-logo.png"
+          alt="logo"
         />
       </div>
       <div className={style.burgerMenu}>
         <button
           className={style.burgerMenuIcon}
           type="button"
-          onClick={() => setIsMenuOpen(true)}
+          onClick={toggleMenu}
         >
           <i className="fas fa-bars fa-2x"></i>
         </button>
       </div>
-      <DisplayMenu isOpen={isMenuOpen} />
+      <DisplayMenu
+        animateMenu={animateMenu}
+        toggleMenu={toggleMenu}
+        isLoggedIn={isLoggedIn}
+      />
     </>
   );
 }
